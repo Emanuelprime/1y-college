c56aab86a29de26a6eb0d3be8c24edaab21624fb
Emanuelprime <emanuelll3000@gmail.com>
2023-10-02 17:30:40 -0300

    att

--- arquivos.py
+++ arquivos.py
@@ -1,21 +1,17 @@
 import random
 
-nomes = ['Ana', 'João', ' Maria', 'Pedro', 'Julia', 'Lucas', 'Beatriz', 'Guilherme', 'Isabela', 'Matheus',
-         'Sophia', 'Rafael', 'Laura', 'Gabriel', 'Manuela', 'Leonardo', 'Valentina', 'Felipe', 'Luiza Enzo']
+nomes = ['Ana', 'João', 'Maria', 'Pedro', 'Julia', 'Lucas', 'Beatriz', 'Guilherme', 'Isabela', 'Matheus',
+         'Sophia', 'Rafael', 'Laura', 'Gabriel', 'Manuela', 'Leonardo', 'Valentina', 'Felipe', 'Luiza', 'Enzo']
 
 sobrenomes = ["Silva", "Santos", "Oliveira", "Souza", "Rodrigues", "Ferreira", "Almeida", "Pereira", "Gomes", "Costa",
               "Carvalho", "Martins", "Ribeiro", "Lima", "Araújo", "Cruz", "Mendes", "Nascimento", "Fernandes", "Cardoso"]
 
 # Gerar uma idade aleatória entre 16 e 69 anos
-
-
 def gerar_idade():
     return random.randint(16, 69)
 
 # Função principal para criar o arquivo com N nomes completos e idades aleatórias
-
-
-def criar_arquivo_com_dados_aleatorios(N, nome_do_arquivo):
+def criar_arquivo_com_dados_aleatorios(N, nome_do_arquivo="nome_completo_idade.txt"):
     with open(nome_do_arquivo, 'w') as arquivo:
         for _ in range(N):
             nome = random.choice(nomes)
@@ -25,14 +21,10 @@ def criar_arquivo_com_dados_aleatorios(N, nome_do_arquivo):
             linha = f"{nome_completo},{idade}\n"
             arquivo.write(linha)
 
-
 if __name__ == "__main__":
     try:
         N = int(input("Digite o número de nomes e idades a serem gerados: "))
-        nome_do_arquivo = input("Digite o nome do arquivo de saída: ")
-
-        criar_arquivo_com_dados_aleatorios(N, nome_do_arquivo)
-        print(
-            f"{N} nomes completos e idades aleatórias foram gerados e escritos em '{nome_do_arquivo}'.")
+        criar_arquivo_com_dados_aleatorios(N)
+        print(f"{N} nomes completos e idades aleatórias foram gerados e escritos em 'nome_completo_idade.txt'.")
     except ValueError:
         print("Por favor, insira um número válido para N.")
