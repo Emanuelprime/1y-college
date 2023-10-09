9894349fcb14f27605ff58611c70e56c1891c97a
Emanuelprime <emanuelll3000@gmail.com>
2023-10-09 16:52:26 -0300

    att

--- Aula 10 arquivos/arquivos4.py
+++ Aula 10 arquivos/arquivos4.py
@@ -1,30 +1,25 @@
-def exercicio4(arquivoNomes, arquivoNotas):
-    nomes = open(arquivoNomes, 'r', encoding="utf-8")
-    notas = open(arquivoNotas, 'r', encoding="utf-8")
-    arquivoNomesNotas = open("4-listaMediasAlunos.txt", 'w+', encoding="utf-8")
+def calcular_media(notas):
+    notas = [float(nota) for nota in notas]  # Converte as notas de strings para floats
+    return sum(notas) / len(notas)
 
-    linhaNotas = notas.readline()
-    linhaNomes = nomes.readline()
+def gerar_arquivo_com_medias(arquivo_alunos, arquivo_notas, arquivo_medias):
+    try:
+        with open(arq_alunos, 'r') as alunos_file, open(arq_notas, 'r') as notas_file, open(arquivo_medias, 'w') as medias_file:
+            for nome, notas_str in zip(alunos_file, notas_file):
+                nome = nome.strip()
+                notas = notas_str.split()
+                media = calcular_media(notas)
+                medias_file.write(f"{nome}: {media:.2f}\n")
+        print(f"As médias foram calculadas e escritas em '{arquivo_medias}'.")
+    except FileNotFoundError:
+        print("Um dos arquivos não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {str(e)}")
 
-    while linhaNotas != "":
-        listaNotas = linhaNotas.split(" ")
-        nome = linhaNomes.replace("\n", "")
-        soma = 0.0
-        
-        for item in listaNotas:
-            indice = listaNotas.index(item)
-            if "\n" in item:
-                listaNotas[indice] = listaNotas[indice].replace("\n", "")
-            soma += float(listaNotas[indice])
-                
-        media = "{:.2f}".format(soma / 3)
-        alunoMedia = f'{nome:<20} {media}\n'
-        arquivoNomesNotas.write(alunoMedia)
-        linhaNotas = notas.readline()
-        linhaNomes = nomes.readline()
-        
-    arquivoNomesNotas.close()
-    nomes.close()
-    notas.close()
-    
-exercicio4("4-listaNomesAlunos.txt", "4-listaNotasAlunos.txt")
+if __name__ == "__main__":
+    arq_alunos = input("Digite o nome do arquivo com os nomes dos alunos: ")
+    arq_notas = input("Digite o nome do arquivo com as notas dos alunos: ")
+    arquivo_medias = input("Digite o nome do arquivo de destino para as médias: ")
+
+    gerar_arquivo_com_medias(arq_alunos, arq_notas, arquivo_medias)
+
