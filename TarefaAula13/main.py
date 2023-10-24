0dfe4fc5531a01516e87f49a0411a325d7aefc8a
Emanuelprime <emanuelll3000@gmail.com>
2023-10-23 23:14:03 -0300

    att

--- TarefaAula13/main.py
+++ TarefaAula13/main.py
@@ -1,21 +1,21 @@
-#1 Crie uma classe chamada Carro com atributos encapsulados (_modelo e _ano) e métodos para obter e definir esses atributos.
-from Carros import Carros
 
-c1 = Carros('Fusca',1938)
-print("Exercicio 1", c1._Carros__modelo, c1._Carros__ano)
+from Veiculos import Carro, Moto, Veiculos, Carros
 
-#2 Modifique a classe Carro do exercício anterior para usar atributos privados (__modelo e __ano) e métodos de acesso para obter e definir esses atributos.
-c1.ano = 2000
-c1.modelo = "Golf"
-print("Exercicio 2", c1.modelo, c1.ano)
+# Exemplo de uso
+# Exercício 1
+c1 = Carros('Fusca', 1938)
+print("Exercício 1:", c1.get_modelo(), c1.get_ano())
 
-#3 Crie uma classe Veiculo com atributos como tipo e velocidade e, em seguida, crie classes Carro e Moto que herdem de Veiculo.
-from Veiculos import Carro, Moto
+# Exercício 2
+c1.set_ano(2000)
+c1.set_modelo("Golf")
+print("Exercício 2:", c1.get_modelo(), c1.get_ano())
+
+# Exercício 3
 c2 = Carro("Conversível", 240, "TEste")
 m1 = Moto("Esportiva", 200, "teste")
-print("Exercicio 3 Carro:", c2._tipo, c2._velocidade)
-print("Exercicio 3 Moto:", m1._tipo, m1._velocidade)
+print("Exercício 3 Carro:", c2._tipo, c2._velocidade)
+print("Exercício 3 Moto:", m1._tipo, m1._velocidade)
 
-#4 Na classe Veiculo, crie um método chamado descricao() que imprima uma descrição básica do veículo. Sobrescreva esse método nas classes Carro e Moto para fornecer descrições específicas para cada tipo de veículo.
-
-c2.descricao = "teste"
+# Exercício 4
+c2.descricao()
