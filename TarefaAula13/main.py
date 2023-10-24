e0c65725e1ae73372443032ec308eda977234970
Emanuelprime <emanuelll3000@gmail.com>
2023-10-23 23:29:06 -0300

    att

--- TarefaAula13/main.py
+++ TarefaAula13/main.py
@@ -2,22 +2,22 @@
 from Veiculos import Carro, Moto, Veiculos, Carros, acelerar
 
 # Exemplo de uso
-# Exercício 1
+# Exercicio 1
 c1 = Carros('Fusca', 1938)
 print("Exercício 1:", c1.get_modelo(), c1.get_ano())
 
-# Exercício 2
+# Exercicio 2
 c1.set_ano(2000)
 c1.set_modelo("Golf")
 print("Exercício 2:", c1.get_modelo(), c1.get_ano())
 
-# Exercício 3
+# Exercicio 3
 c2 = Carro("Conversível", 240, "AUDI")
 m1 = Moto("Esportiva", 200, "1200")
 print("Exercício 3 Carro:", c2._tipo, c2._velocidade)
 print("Exercício 3 Moto:", m1._tipo, m1._velocidade)
 
-# Exercício 4
+# Exercicio 4
 c2.descricao()
 m1.descricao()
 
