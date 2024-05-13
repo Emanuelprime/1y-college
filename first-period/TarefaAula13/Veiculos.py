2ec67e21fbf27454858dd1bb30c5b0f6e3cd3aa5
Emanuelprime <emanuelll3000@gmail.com>
2024-05-13 19:12:14 -0300

    att

---
+++ first-period/TarefaAula13/Veiculos.py
@@ -0,0 +1,56 @@
+# Carros.py
+class Carros:
+    def __init__(self, modelo, ano):
+        self.__modelo = modelo
+        self.__ano = ano
+
+    def get_modelo(self):
+        return self.__modelo
+
+    def set_modelo(self, modelo):
+        self.__modelo = modelo
+
+    def get_ano(self):
+        return self.__ano
+
+    def set_ano(self, ano):
+        self.__ano = ano
+
+
+# Veiculos.py
+
+
+class Veiculos:
+    def __init__(self, tipo, velocidade):
+        self._tipo = tipo
+        self._velocidade = velocidade
+
+    def descricao(self):
+        print(f"Tipo de veículo: {self._tipo}")
+        print(f"Velocidade: {self._velocidade} km/h")
+
+
+class Carro(Veiculos):
+    def __init__(self, tipo, velocidade, marca):
+        super().__init__(tipo, velocidade)
+        self._marca = marca
+
+    def descricao(self):
+        super().descricao()
+        print(f"Marca: {self._marca}")
+        print("Este veículo é um carro.")
+
+
+class Moto(Veiculos):
+    def __init__(self, tipo, velocidade, cilindrada):
+        super().__init__(tipo, velocidade)
+        self._cilindrada = cilindrada
+
+    def descricao(self):
+        super().descricao()
+        print(f"Cilindrada: {self._cilindrada} cc")
+        print("Este veículo é uma moto.")
+
+
+def acelerar(veiculos):
+    veiculos._velocidade += 10  # Aumenta a velocidade em 10 km/h
---
+++ first-period/TarefaAula13/main.py
@@ -0,0 +1,31 @@
+
+from Veiculos import Carro, Moto, Veiculos, Carros, acelerar
+
+# Exercicio 1
+c1 = Carros('Fusca', 1938)
+print("Exercício 1:", c1.get_modelo(), c1.get_ano())
+
+# Exercicio 2
+c1.set_ano(2000)
+c1.set_modelo("Golf")
+print("Exercício 2:", c1.get_modelo(), c1.get_ano())
+
+# Exercicio 3
+c2 = Carro("Conversível", 240, "AUDI")
+m1 = Moto("Esportiva", 200, "1200")
+print("Exercício 3 Carro:", c2._tipo, c2._velocidade)
+print("Exercício 3 Moto:", m1._tipo, m1._velocidade)
+
+# Exercicio 4
+c2.descricao()
+m1.descricao()
+
+# Exercicio 5
+print("Velocidade inicial do carro:", c2._velocidade, "km/h")
+print("Velocidade inicial da moto:", m1._velocidade, "km/h")
+
+acelerar(c2)
+acelerar(m1)
+
+print("Velocidade após acelerar o carro:", c2._velocidade, "km/h")
+print("Velocidade após acelerar a moto:", m1._velocidade, "km/h")
